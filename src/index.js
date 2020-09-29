12ec58e715518c76ead52b47021c7bf9b467cdad
kelibst <kboster17@gmail.com>
2020-09-29 22:28:22 +0000

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
 import { createStore } from 'redux';
 import rootReducer from './reducers/index';
 import App from './components/App';
